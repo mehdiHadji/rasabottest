d7ce1aa3ada81bc32b578bdd90a7bc8d1aab9709
mehdiHadji <mannnonegra@gmail.com>
2019-10-14 19:53:56 +0200

    add validation to slots

--- actions.py
+++ actions.py
@@ -19,64 +19,53 @@ def slackitems(tracker):
 
 
 class CertificateForm(FormAction):
-    """Example of a custom form action"""
 
     def name(self) -> Text:
-        """Unique identifier of the form"""
-
         return "certificate_form"
 
+    
     @staticmethod
     def required_slots(tracker: Tracker) -> List[Text]:
-        """A list of required slots that the form has to fill"""
-        return ["certif_type", "period", "purpose", "details", "langue"]
+        if tracker.get_slot("certif_type") == "expense report":
+            return ["period","purpose","details"]
+        if tracker.get_slot("certif_type") == "work certificate" or tracker.get_slot("certif_type") == "salary certificate":
+            return ["langue","copies_num"]
+        if tracker.get_slot("certif_type") == "mission order":
+            return ["endroit","period","purpose"]
+        else:
+            return ["certif_type", "period", "purpose", "details", "langue","endroit"]
 
+    
     def slot_mappings(self) -> Dict[Text, Union[Dict, List[Dict]]]:
-        """A dictionary to map required slots to
-            - an extracted entity
-            - intent: value pairs
-            - a whole message
-            or a list of them, where a first match will be picked"""
-
         return {
             "certif_type": self.from_entity(entity="certif_type", not_intent="chitchat"),
             "period": [self.from_entity(entity="period"), self.from_text()],
             "purpose": [self.from_entity(entity="purpose"), self.from_text()],
             "details": [self.from_entity(entity="details"), self.from_text()],
             "langue": [self.from_entity(entity="langue"), self.from_text()],
+            "copies_num": [self.from_entity(entity="copies_num"), self.from_text()],
+            "endroit": [self.from_entity(entity="endroit"), self.from_text()],
         }
 
 
     @staticmethod
     def certif_db() -> List[Text]:
-        return [
-            "expense report",
-            "leave authorization",
-            "mission order",
-            "pay statement",
-            "salary certificate",
-            "work certificate",
-        ]
+        return ["expense report","leave authorization","mission order","pay statement","salary certificate","work certificate",]
 
+    
     @staticmethod
     def period_db() -> List[Text]:
-        return [
-            "tomorrow",
-            "tomorrow morning",
-            "30-10-2019",
-            "next monday",
-            "next week",
-            "next month",
-        ]
+        return ["tomorrow","tomorrow morning","30-10-2019","next monday","next week","next month",]
 
+    
     @staticmethod
     def langue_db() -> List[Text]:
-        return [
-            "fr",
-            "en",
-            "french",
-            "english",
-        ]
+        return ["fr","en","french","english",]
+
+
+    @staticmethod
+    def endroit_db() -> List[Text]:
+        return ["rabat","casablanca","agadir",]
 
 
     @staticmethod
@@ -124,11 +113,40 @@ class CertificateForm(FormAction):
         domain: Dict[Text, Any],
     ) -> Dict[Text, Any]:
 
-        if value.lower() in self.langue_db():
-            return {"langue": True}
+        if value in self.langue_db():
+            return {"langue": value}
         else:
             return dispatcher.utter_template("utter_wrong_langue", tracker)
-                
+    
+
+    def validate_copies_num(
+        self,
+        value: Text,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> Dict[Text, Any]:
+
+        if self.is_int(value) and int(value) > 0:
+            return {"copies_num": value}
+        else:
+            dispatcher.utter_template("utter_wrong_copies_num", tracker)
+            return {"copies_num": None}
+
+
+    def validate_endroit(
+        self,
+        value: Text,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> Dict[Text, Any]:
+
+        if value in self.endroit_db():
+            return {"endroit": value}
+        else:
+            return dispatcher.utter_template("utter_wrong_endroit", tracker)
+
 
     def submit(
         self,
